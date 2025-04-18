0d16f2605e6f945d203eae87bc438dfad1d86b73
c0d33ngr <jeffwhewhetu@gmail.com>
2025-04-18 19:33:35 +0100

    update code

--- qux/src/qux/Qux.java
+++ qux/src/qux/Qux.java
@@ -3,7 +3,7 @@ package qux;
 public class Qux {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("This is a test");
-        System.out.println("This is a test");
+        System.out.println("This is a test 5");
+        System.out.println("This is a test 5");
       }
 }
